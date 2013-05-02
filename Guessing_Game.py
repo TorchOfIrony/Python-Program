3800e4f9c437f6bf45b36994adb79f5cf3e16840
TorchOfIrony <torchanderson@aol.com>
2013-05-01 21:51:20 -0400

    Edited Guessing_Game.py

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,28 +1,53 @@
 import random
 
-guesses_made = 0
+def GuessingGame():
 
-name = raw_input('Hello! What is your name?\n')
+	guesses_made = 0
 
-number = random.randint(1, 20)
-print 'Well, {0}, I am thinking of a number between 1 and 20.'.format(name)
+	name = raw_input('Hello! What is your name?\n')
 
-while guesses_made < 6:
+	number = random.randint(1, 20)
+	
+	willToContinue = True
+	choiceOfContinue
+	
+	print 'Well, {0}, I am thinking of a number between 1 and 20.'.format(name)
+	while willToContinue == True:
+	
+		while guesses_made < 6:
 
-    guess = int(raw_input('Take a guess: '))
+			guess = int(raw_input('Take a guess: '))
 
-    guesses_made += 1
+			guesses_made += 1
 
-    if guess < number:
-        print 'Your guess is too low.'
+			if guess < number:
+				print 'Your guess is too low.'
 
-    if guess > number:
-        print 'Your guess is too high.'
+			if guess > number:
+				print 'Your guess is too high.'
 
-    if guess == number:
-        break
+			if guess == number:
+				break
 
-if guess == number:
-    print 'Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made)
-else:
-    print 'Nope. The number I was thinking of was {0}'.format(number)
+		if guess == number:
+		
+			print 'Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made)
+			
+		else:
+		
+			print 'Nope. The number I was thinking of was {0}'.format(number)
+			
+		choiceOfContinue = raw_input('So, {0}... want to play another round?(Type "yes", "y", or "no", "n")\n'.format(name))
+		
+		if choiceOfContinue == 'yes' *or* choiceOfContinue == 'y':
+		
+			willToContinue = True
+			
+		elif choiceOfContinue == 'no' *or* choiceOfContinue == 'n':
+		
+			willToContinue = False
+			
+		else
+		
+			willToContinue = False
+		
